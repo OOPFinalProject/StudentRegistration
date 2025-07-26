a8c7e54c7c89e6af3b3fd110748118305d1e13b5
Nebiyu Samuel <nebiyusamuel809@gmail.com>
2025-07-27 00:14:20 +0300

    feat: Add Main entry point to launch the student registration system
    - Displays welcome message
    - Launches MainMenu UI

---
+++ ui/Main.java
@@ -0,0 +1,11 @@
+package ui;
+import ui.MainMenu;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("****Welcome to StuReg Students Registration and Management System****");
+        new MainMenu().show();
+
+        
+    }
+}
