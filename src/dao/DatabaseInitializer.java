87faf0026bcde2cc47a54dfe5cc20c58684ae34b
Hamza Mohammed <hmmhsd37@gmail.com>
2025-07-28 19:31:46 +0300

    structure folder to use maven for compiling and running with jdbc

---
+++ src/dao/DatabaseInitializer.java
@@ -0,0 +1,47 @@
+package dao;
+
+import java.sql.Connection;
+import java.sql.Statement;
+
+public class DatabaseInitializer {
+    public static void initialize() {
+        try (Connection conn = DBConnection.getConnection(); Statement stmt = conn.createStatement()) {
+            String studentsTable = """
+                CREATE TABLE IF NOT EXISTS students (
+                    id INTEGER PRIMARY KEY AUTOINCREMENT,
+                    student_id TEXT UNIQUE,
+                    name TEXT,
+                    major TEXT,
+                    year INTEGER
+                );
+            """;
+
+            String coursesTable = """
+                CREATE TABLE IF NOT EXISTS courses (
+                    id INTEGER PRIMARY KEY AUTOINCREMENT,
+                    course_id TEXT UNIQUE,
+                    course_name TEXT,
+                    credit_hours INTEGER
+                );
+            """;
+
+            String registrationTable = """
+                CREATE TABLE IF NOT EXISTS registration (
+                    id INTEGER PRIMARY KEY AUTOINCREMENT,
+                    student_id TEXT,
+                    course_id TEXT UNIQUE,
+                    FOREIGN KEY (student_id) REFERENCES students(student_id) ON DELETE CASCADE ,
+                    FOREIGN KEY (course_id) REFERENCES courses(course_id)
+                );
+            """;
+
+            stmt.execute(studentsTable);
+            stmt.execute(coursesTable);
+            stmt.execute(registrationTable);
+
+            System.out.println("✅ Database tables created.");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
