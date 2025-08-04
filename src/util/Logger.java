5a8d70d7673fad04811a787a31c595c75a395dc9
Hamza Mohammed <hmmhsd37@gmail.com>
2025-08-04 22:49:58 +0300

    minor fix

--- src/util/Logger.java
+++ src/util/Logger.java
@@ -8,7 +8,7 @@ import java.io.IOException;
 
 
 public class Logger implements Loggable {
-
+    @Override
     public  void log(String entry, String FileName){
         try (BufferedWriter bw = new BufferedWriter(new FileWriter(FileName+".txt",true))) {
             bw.write(entry+"\n");
@@ -19,7 +19,7 @@ public class Logger implements Loggable {
        
     }
 }
-
+    @Override
     public  void read(String FileName){
         try (BufferedReader reader = new BufferedReader(new FileReader(FileName+".txt"))) {
             String line;
